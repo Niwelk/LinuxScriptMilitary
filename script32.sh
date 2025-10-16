46c7a00416907ba491a64df841824971e9ef2eee
L1ghtOfQuazar <feodorgan@gmail.com>
2025-10-16 15:29:58 +0300

    final merge

--- script32.sh
+++
@@ -1,9 +0,0 @@
-echo "Step 3: Удаление файлов..."
-while IFS= read -r file; do
-    if [ -f "$file" ]; then
-        rm -f "$file"
-        echo "Удален и добавлен в архив: $file"
-    fi
-done < "$F_LIST"
-find "$F_PATH" -type d -empty -delete
-echo "Архивирование и удаление файлов прошло успешно."
--- scriptmain.sh
+++ scriptmain.sh
@@ -1,3 +1,40 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "ERROR: Введите хотя бы 1 аргумент!"
+    exit 1
+fi
+if [ $# -gt 4 ]; then
+    echo "ERROR: Введено слишком много аргументов!"
+    exit 1
+fi
+F_PATH="$1"
+if [ ! -d "$F_PATH" ]; then
+    echo "ERROR: Папка '$F_PATH' не существует!" >&2
+    exit 1
+fi
+if [ ! -x "/usr/bin/bc" ]; then
+    echo "ERROR: bc не установлен в системе. Для работы необходимо установить его через: sudo apt install bc." >&2
+    exit 1
+fi
+FOLDER_S=$(du -sb "$F_PATH" 2>/dev/null | cut -f1)
+FS_S=$(df -B1 "$F_PATH" 2>/dev/null | awk 'NR==2 {print $2}')
+if [ -z "$FOLDER_S" ] || [ -z "$FS_S" ]; then
+    echo "Ошибка: Не удалось вычислить размеры!" >&2
+    exit 1
+fi
+PERCENT=$(awk "BEGIN {printf \"%.6f\", ($FOLDER_S * 100) / $FS_S}")
+if [ $(echo "$PERCENT >= 0.1" | bc -l 2>/dev/null) -eq 1 ]; then
+    NEW=$(awk "BEGIN {printf \"%.1f\", $PERCENT}")
+    echo "Заполненность папки: ${NEW}%"
+elif [ $(echo "$PERCENT >= 0.001" | bc -l 2>/dev/null) -eq 1 ]; then
+    NEW=$(awk "BEGIN {printf \"%.3f\", $PERCENT}")
+    echo "Заполненность папки: ${NEW}%"
+else
+    NEW=$(awk "BEGIN {printf \"%.2e\", $PERCENT}")
+    NEW=$(echo "$NEW" | sed 's/e/ x 10^/; s/+//; s/-/⁻/')
+    echo "Заполненность папки: ${NEW}%"
+    NEW=$(awk "BEGIN {printf \"%.11f\", $PERCENT}")
+fi                                                                                                                                    
 if [ $# -lt 5 ] && [ $# -gt 1 ]; then
     if [ $(echo "$NEW > $3" | bc -l 2>/dev/null) -eq 1 ]; then
         AR_C="${3:-70}"
@@ -5,7 +42,6 @@ if [ $# -lt 5 ] && [ $# -gt 1 ]; then
         ARCHIVE_PATH="$2"
         NAME="archive_$(date +%Y%m%d_%H%M%S).tar.gz"
         echo "Критерий архивации выполнен (${NEW}% > ${AR_C}%), архивируем."
-
         F_LIST=$(mktemp)
         find "$F_PATH" -type f -printf "%T@ %p\n" > /tmp/tsf.txt
         sort -n /tmp/tsf.txt > /tmp/tsfs.txt
@@ -18,7 +54,7 @@ if [ $# -lt 5 ] && [ $# -gt 1 ]; then
             exit 1
         fi
         echo "Найдено файлов для архивации: $(wc -l < "$F_LIST")"
-		echo "Step 1: Архивирование..."
+        echo "Step 1: Архивирование..."
         tar -czf "$ARCHIVE_PATH/$NAME" -T "$F_LIST" .
         if [ $? -ne 0 ]; then
             echo "ERROR: Ошибка архивирования!"
