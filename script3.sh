68e9317ea0202e6753f8efecf376bd47cf7f4616
Niwelk <ivan.chervenkov11@gmail.com>
2025-10-16 15:19:16 +0300

    Патч k1: устранение ошибки структуры в script3.sh

--- script3.sh
+++ script3.sh
@@ -1,4 +1,12 @@
-F_LIST=$(mktemp)
+if [ $# -lt 5 ] && [ $# -gt 1 ]; then
+    if [ $(echo "$NEW > $3" | bc -l 2>/dev/null) -eq 1 ]; then
+        AR_C="${3:-70}"
+        FILES_C="${4:-50}"
+        ARCHIVE_PATH="$2"
+        NAME="archive_$(date +%Y%m%d_%H%M%S).tar.gz"
+        echo "Критерий архивации выполнен (${NEW}% > ${AR_C}%), архивируем."
+
+        F_LIST=$(mktemp)
         find "$F_PATH" -type f -printf "%T@ %p\n" > /tmp/tsf.txt
         sort -n /tmp/tsf.txt > /tmp/tsfs.txt
         head -n "$FILE_COUNT" /tmp/tsfs.txt > /tmp/tsfso.txt
@@ -10,11 +18,3 @@ F_LIST=$(mktemp)
             exit 1
         fi
         echo "Найдено файлов для архивации: $(wc -l < "$F_LIST")"
-
-if [ $# -lt 5 ] && [ $# -gt 1 ]; then
-    if [ $(echo "$NEW > $3" | bc -l 2>/dev/null) -eq 1 ]; then
-        AR_C="${3:-70}"
-        FILES_C="${4:-50}"
-        ARCHIVE_PATH="$2"
-        NAME="archive_$(date +%Y%m%d_%H%M%S).tar.gz"
-        echo "Критерий архивации выполнен (${NEW}% > ${AR_C}%), архивируем."
