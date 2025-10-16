436d922d6c61be2089d6845315c8391dd5adf063
Niwelk <ivan.chervenkov11@gmail.com>
2025-10-16 14:55:38 +0300

    проверка условий архивации

--- script3.sh
+++ script3.sh
@@ -10,3 +10,11 @@ F_LIST=$(mktemp)
             exit 1
         fi
         echo "Найдено файлов для архивации: $(wc -l < "$F_LIST")"
+
+if [ $# -lt 5 ] && [ $# -gt 1 ]; then
+    if [ $(echo "$NEW > $3" | bc -l 2>/dev/null) -eq 1 ]; then
+        AR_C="${3:-70}"
+        FILES_C="${4:-50}"
+        ARCHIVE_PATH="$2"
+        NAME="archive_$(date +%Y%m%d_%H%M%S).tar.gz"
+        echo "Критерий архивации выполнен (${NEW}% > ${AR_C}%), архивируем."
