60de9a97c70d3c126b05393f465a0cbd6e093b42
L1ghtOfQuazar <feodorgan@gmail.com>
2025-10-16 15:28:56 +0300

    merge

--- script3.sh
+++ script3.sh
@@ -18,3 +18,30 @@ if [ $# -lt 5 ] && [ $# -gt 1 ]; then
             exit 1
         fi
         echo "Найдено файлов для архивации: $(wc -l < "$F_LIST")"
+		echo "Step 1: Архивирование..."
+        tar -czf "$ARCHIVE_PATH/$NAME" -T "$F_LIST" .
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Ошибка архивирования!"
+            rm -f "$F_LIST"
+            exit 1
+        fi
+        echo "Step 2: Проверка архива..."
+        if tar -tzf "$ARCHIVE_PATH/$NAME" > /dev/null; then
+            echo "Проверка архива прошла успешно."
+            echo "Step 3: Удаление файлов..."
+            while IFS= read -r file; do
+                if [ -f "$file" ]; then
+                    rm -f "$file"
+                    echo "Удален и добавлен в архив: $file"
+                fi
+            done < "$F_LIST"
+            find "$F_PATH" -type d -empty -delete
+            echo "Архивирование и удаление файлов прошло успешно."
+        else
+            echo "ERROR: Архив поврежден!"
+        fi
+        rm -f "$F_LIST"
+    else
+        echo "Критерий архивации не выполнен, выход из программы."
+    fi
+fi
